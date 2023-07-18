645c4169aee5ff4b08013304612ec466e7ca562e
MV-src <matteo.vitaloni739@gmail.com>
2023-07-18 14:40:53 +0200

    Perfect Solution LeetCode Daily Challenge 146

--- 146. LRU Cache/main.cpp
+++ 146. LRU Cache/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <queue>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
-bool cmp(pair<int, pair<int,int>>& a,
+/*bool cmp(pair<int, pair<int, int>>& a,
     pair<int, pair<int, int>>& b)
 {
     return a.second.second < b.second.second;
@@ -15,8 +15,6 @@ struct pair_hash {
         auto h1 = std::hash<T1>{}(p.first);
         auto h2 = std::hash<T2>{}(p.second);
 
-        // Mainly for demonstration purposes, i.e. works but is overly simple
-        // In the real world, use sth. like boost.hash_combine
         return h1 ^ h2;
     }
 };
@@ -53,7 +51,86 @@ public:
     }
 
 
+};*/
+
+class LRUCache {
+public:
+    class Node {
+    public:
+        int key;
+        int val;
+        Node* prev;
+        Node* next;
+
+        Node(int key, int val) {
+            this->key = key;
+            this->val = val;
+        }
+    };
+
+    Node* head = new Node(-1, -1);
+    Node* tail = new Node(-1, -1);
+
+    int cap;
+    unordered_map<int, Node*> m;
+
+    LRUCache(int capacity) {
+        cap = capacity;
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    void addNode(Node* newnode) {
+        Node* temp = head->next;
+
+        newnode->next = temp;
+        newnode->prev = head;
+
+        head->next = newnode;
+        temp->prev = newnode;
+    }
+
+    void deleteNode(Node* delnode) {
+        Node* prevv = delnode->prev;
+        Node* nextt = delnode->next;
+
+        prevv->next = nextt;
+        nextt->prev = prevv;
+    }
+
+    int get(int key) {
+        if (m.find(key) != m.end()) {
+            Node* resNode = m[key];
+            int ans = resNode->val;
+
+            m.erase(key);
+            deleteNode(resNode);
+            addNode(resNode);
+
+            m[key] = head->next;
+            return ans;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if (m.find(key) != m.end()) {
+            Node* curr = m[key];
+            m.erase(key);
+            deleteNode(curr);
+        }
+
+        if (m.size() == cap) {
+            m.erase(tail->prev->key);
+            deleteNode(tail->prev);
+        }
+
+        addNode(new Node(key, value));
+        m[key] = head->next;
+    }
 };
+
+
 
 int main() {
 	return 0;
