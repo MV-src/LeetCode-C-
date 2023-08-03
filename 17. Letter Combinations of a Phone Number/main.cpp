b8dd0fba7206fc0b4e57eb4edb0255e3cec71649
MV-src <matteo.vitaloni739@gmail.com>
2023-08-03 12:59:40 +0200

    LeetCode Daily Challenge 17 little changes

--- 17. Letter Combinations of a Phone Number/main.cpp
+++ 17. Letter Combinations of a Phone Number/main.cpp
@@ -6,19 +6,17 @@ using namespace std;
 
 class Solution {
 public:
-    vector<string> fun(string digits, int size, int i, unordered_map<int, string>& letters) {
+    vector<string> fun(string digits, int size, int i, unordered_map<char, string>& letters) {
         vector<string> res;
-        string j(1, digits[i]);
-        int n = stoi(j);
         if (i == size-1) {
-            for (auto x : letters[n]) {
-                string b(1, x);
-                res.push_back(b);
+            for (auto x : letters[digits[i]]) {
+                string j(1, x);
+                res.push_back(j);
             }
             return res;
         }
         
-        for (auto x : letters[n]) {
+        for (auto x : letters[digits[i]]) {
             vector<string> s = fun(digits, size, i + 1, letters);
             for (auto& y : s) y +=x;
             for (auto y : s) res.push_back(y);
@@ -29,15 +27,15 @@ public:
     vector<string> letterCombinations(string digits) {
         int size = digits.size();
         reverse(digits.begin(), digits.end());
-        unordered_map<int, string> letters;
-        letters[2] = "abc";
-        letters[3] = "def";
-        letters[4] = "ghi";
-        letters[5] = "jkl";
-        letters[6] = "mno";
-        letters[7] = "pqrs";
-        letters[8] = "tuv";
-        letters[9] = "wxyz";
+        unordered_map<char, string> letters;
+        letters['2'] = "abc";
+        letters['3'] = "def";
+        letters['4'] = "ghi";
+        letters['5'] = "jkl";
+        letters['6'] = "mno";
+        letters['7'] = "pqrs";
+        letters['8'] = "tuv";
+        letters['9'] = "wxyz";
         if (size == 0) return {};
         return fun(digits,size,0,letters);
     }
